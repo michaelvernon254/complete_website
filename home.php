62fe31f83c49c583b50deb08cae9b1a120b0e12d
vernonski <35599983+vernonski@users.noreply.github.com>
2018-02-23 16:53:28 +0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>INDEX</title>
+	<link rel="stylesheet" type="text/css" href="syle.css">
+</head>
+<body>
+<h1>WELCOME TO THE LUCKY DRAW CHALLENGE!!!!</h1>
+<h2>Please input your name to proceed to the challenge.</h2>
+<form method="GET" action="vernon.php">
+	<input type="text" name="txt">
+<h2>Input your lucky number between 1 and 6 into the box.</h2>
+<form method="GET" action="vernon.php">
+<P>Please select a number between 1-6 and win</P>
+<input type="number" name="bet">
+<button class="btn">Submit</button>
+	</form>	
+</body> 	
+</html>
